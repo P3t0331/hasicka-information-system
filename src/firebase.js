62831b5ac711276b4df9733c96bf58b96257a300
Gregus <peter.gregus@sap.com>
2026-05-25 17:28:15 +0200

    update

--- src/firebase.js
+++ src/firebase.js
@@ -16,6 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
+  experimentalAutoDetectLongPolling: true,
   localCache: persistentLocalCache({ tabManager: persistentMultipleTabManager() })
 });
